111ac4997475ebd9338f8298b7259b9134c18881
Quinn Patwardhan <quinnpatwardhan@quinns-mbp.lan>
2022-07-26 11:53:52 -0400

    first commit

---
+++ src/components/Explorer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SearchSection from "./SearchSection";
+
+export default function Explorer() {
+  return (
+    <div className="explorer-wrapper">
+      <SearchSection />
+    </div>
+  );
+}
---
+++ src/pages/HomeScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Explorer from "../components/Explorer";
+
+export default function HomeScreen() {
+  return (
+    <div>
+      <Explorer />
+    </div>
+  );
+}
